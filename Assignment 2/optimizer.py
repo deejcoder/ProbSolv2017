19d3f9cf82e9e7194d63ebcefb68bf707f5f1c5f
oneOCT3T <dylant@live.co.uk>
2017-05-30 01:17:15 +1200

    ass2

--- Assignment 2/optimizer.py
+++ Assignment 2/optimizer.py
@@ -3,96 +3,117 @@ from building import *
 # speed test - use "python optimizer.py" to run
 if __name__ == "__main__":
     import timeit
-    test_size = 20 # set to 100 to check time for speed race
+    test_size = 50 # set to 100 to check time for speed race
     t1 = timeit.repeat(stmt="optimizer.max_food(b)", setup="import gc, building, optimizer; b = building.random_building({0}, True); gc.collect()".format(test_size), repeat=3, number=1)
     t2 = timeit.repeat(stmt="optimizer.max_supplies(b)", setup="import gc, building, optimizer; b = building.random_building({0}, False); gc.collect()".format(test_size), repeat=3, number=1)
     # some calculation that takes ~1 sec on my machine
     tref = timeit.repeat(stmt="for i in range(1000000): a=i^2", setup="import gc; gc.collect()", repeat=3, number=19)
     print("max_food(n={0}) = {1} ({3} normalized), max_supplies(n={0}) = {2} ({4} normalized)".format(test_size, min(t1), min(t2), min(t1) / min(tref), min(t2) / min(tref)))
 
-def split( grid ):
-	length = len(grid[0])
-	topleft = []
-	for ind in range(0,(length//2)+1):
-		topleft.append(grid[ind][:(length//2)+1])
-	topright = []
-	for ind in range(0,(length//2)+1):
-		topright.append(grid[ind][(length//2):])
-	bottomleft = []
-	for ind in range((length//2), length):
-		bottomleft.append(grid[ind][:(length//2)+1])
-	bottomright = []
-	for ind in range((length//2), length):
-		bottomright.append(grid[ind][(length//2):])
 
-	return topleft, topright, bottomleft, bottomright
+"""
+    METHOD: Breaks the grid into four quadrants so that each quadrant may be 
 
-def trans_BL(inp):
-	output = []
-	for row in inp:
-		row.reverse()
-		output.append(row)
-	return output
+"""
 
-def trans_TL(inp):
-	output = []
-	inp.reverse()
-	for row in inp:
-		row.reverse()
-		output.append(row)
-	return output
+def getQuadrants( grid, size ):
 
-def trans_TR(inp):
-	inp.reverse()
-	return inp
+    length = len(grid[0])
+
+    tl = []
+    for i in range( size + 1 ):
+        tl.append(grid[i][:size + 1])
+
+    tr = []
+    for i in range( size + 1):
+        tr.append(grid[i][size:])
+
+    bl = []
+    for i in range( size, length ):
+        bl.append( grid[i][:size + 1] )
+
+    br = []
+    for i in range( size, length):
+        br.append(grid[i][size:])
+
+    return tl, tr, bl, br
+
+"""
+    METHODS: to normalize the quadrant such that
+    they're all in the same form, with @ at row 0, column 0
+    Bottom right is already in the right form.
+
+"""
+
+def normalizeBottomLeft( quad ):
+    normal = []
+
+    for row in quad:
+        """
+            The bottom left needs its columns
+            reversed such that @ is at 0,0
+        """
+        row.reverse()
+        normal.append( row )
+    
+    return normal
+
+def normalizeTopLeft( quad ):
+
+    normal = []
+
+    """
+        The top left needs its rows and columns are reversed
+        such that @ is at 0,0
+    """
+
+    quad.reverse()
+
+    for row in quad:
+        row.reverse()
+        normal.append( row )
+
+    return normal
+
+def normalizeTopRight( quad ):
+
+    """
+        Simply reversing the rows will place @ at 0,0
+    """
+    quad.reverse()
+
+
+    return quad
+
 
 def max_food(building):
     """returns the maximum number of food that can be collected from given building"""
     grid = building.rooms
    # n = building.size*2 + 1
-    #size = building.size
+    size = building.size
 
     #S = grid
     #S[size][size] = Supplies( -float('infinity' ))
     #max_food_quadrant( building, split_grid( grid, 0, size ), size )
 
     S = [[ item.food for item in row ] for row in grid]
-    lists = split(S)
-    topleft = trans_TL(lists[0])
-    topright = trans_TR(lists[1])
-    bottomleft = trans_BL(lists[2])
-    bottomright = lists[3]
 
-    """for i in topleft:
-        print([item.food for item in i])
-        
-    print("")
-    for i in topright:
-        print([item.food for item in i])
+    # Split the quadrants up
+    lists = getQuadrants( S, size )
 
-    print("")
-    for i in bottomleft:
-        print([item.food for item in i])
-
-    print("") 
-    for i in bottomright:
-        print([item.food for item in i])"""
+    # Transform the quadrants, br already in form
+    br = lists[3]
+    tl = normalizeTopLeft( lists[0] )
+    tr = normalizeTopRight( lists[1] )
+    bl = normalizeBottomLeft( lists[2] )
 
 
-    #print( split_grid( grid, 0, size ) )
-    """
-   for i in topleft:
-        print( [item for item in i] )
-    print("")
-    """
-
-
-
-    tl = max_food_quadrant( topleft )
-    tr = max_food_quadrant( topright )
-    bl = max_food_quadrant( bottomleft )
-    br = max_food_quadrant( bottomright )
-    return max( tl, tr, bl, br )
+    # Get the best solution in the form [(food,water),...]
+    poss_tl = max_food_quadrant( tl )
+    poss_tr = max_food_quadrant( tr )
+    poss_bl = max_food_quadrant( bl )
+    poss_br = max_food_quadrant( br )
+    return max( poss_tl, poss_tr, poss_bl, poss_br )
    # return building.size * 10 # dummy implementation - replace
 
 
@@ -142,18 +163,24 @@ def max_supplies(building):
     """returns the maximum of min(food,water) that can be collected from given building"""
 
     grid = building.rooms
-    S = [[ [item.food, item.water] for item in row ] for row in grid]
-    lists = split(S)
-    topleft = trans_TL(lists[0])
-    topright = trans_TR(lists[1])
-    bottomleft = trans_BL(lists[2])
-    bottomright = lists[3]
+    size = building.size
 
-    tl = max_supplies_quadrant( topleft )
-    tr = max_supplies_quadrant( topright )
-    bl = max_supplies_quadrant( bottomleft )
-    br = max_supplies_quadrant( bottomright )
-    best = bestTuple( br )
+    S = [[ [item.food, item.water] for item in row ] for row in grid]
+
+    # Split the quadrants up
+    lists = getQuadrants( S, size )
+
+    # Transform the quadrants, br already in form
+    br = lists[3]
+    tl = normalizeTopLeft( lists[0] )
+    tr = normalizeTopRight( lists[1] )
+    bl = normalizeBottomLeft( lists[2] )
+
+    poss_tl = max_supplies_quadrant( tl )
+    poss_tr = max_supplies_quadrant( tr )
+    poss_bl = max_supplies_quadrant( bl )
+    poss_br = max_supplies_quadrant( br )
+    best = bestTuple( poss_tl + poss_tr + poss_bl + poss_br  )
     #print( best )
     print( best )
     return min( best[0], best[1] )
@@ -219,15 +246,11 @@ def max_supplies_quadrant(quad):
                    if( add ):
                        tmp.append( adding )
               possibilities[(row,col)] = tmp
-              #print( quad[row][col], tmp )
-
-    #for f,w in possibilities.items():
-
-       #print("f,w:",f, w)
    
-
     return possibilities[(len(quad)-1,(len(quad)-1))]
     #print("Final square possibilities:", possibilities[(len(quad)-1,(len(quad)-1))])
+
+    
 def bestTuple( list ):
     best = (0,0)
     for i in range( 0, len( list ) ):
